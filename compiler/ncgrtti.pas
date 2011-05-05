chadilukito/fpc-svn

commit e0418f0e1913beca320679f6b28568cf037fbc23

compiler: write not dummy unit name for enumeration and dynamic array rtti
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@14511 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/ncgrtti.pas
@@ -436,10 +436,12 @@ implementation
           current_asmdata.asmlists[al_rtti].concat(Tai_const.Create_32bit(def.max));
           if (tf_requires_proper_alignment in target_info.flags) then
             current_asmdata.asmlists[al_rtti].concat(Cai_align.Create(sizeof(TConstPtrUint)));
+          { write base type }
           if assigned(def.basedef) then
             current_asmdata.asmlists[al_rtti].concat(Tai_const.Create_sym(ref_rtti(def.basedef,rt)))
           else
             current_asmdata.asmlists[al_rtti].concat(Tai_const.create_sym(nil));
+          { write name list }
           hp:=tenumsym(def.firstenum);
           while assigned(hp) do
             begin
@@ -447,7 +449,9 @@ implementation
               current_asmdata.asmlists[al_rtti].concat(Tai_string.Create(hp.realname));
               hp:=hp.nextenum;
             end;
-          current_asmdata.asmlists[al_rtti].concat(Tai_const.Create_8bit(0));
+          { write unit name }
+          current_asmdata.asmlists[al_rtti].concat(Tai_const.Create_8bit(length(current_module.realmodulename^)));
+          current_asmdata.asmlists[al_rtti].concat(Tai_string.Create(current_module.realmodulename^));
         end;
 
         procedure orddef_rtti(def:torddef);
@@ -603,8 +607,9 @@ implementation
                  current_asmdata.asmlists[al_rtti].concat(Tai_const.Create_sym(ref_rtti(def.elementdef,rt)))
                else
                  current_asmdata.asmlists[al_rtti].concat(Tai_const.Create_pint(0));
-               { dummy DynUnitName }
-               current_asmdata.asmlists[al_rtti].concat(Tai_const.Create_8bit(0));
+               { write unit name }
+               current_asmdata.asmlists[al_rtti].concat(Tai_const.Create_8bit(length(current_module.realmodulename^)));
+               current_asmdata.asmlists[al_rtti].concat(Tai_string.Create(current_module.realmodulename^));
              end;
         end;
 

rtl/objpas/typinfo.pp
@@ -107,7 +107,8 @@ unit typinfo;
                       tkEnumeration:
                         (
                         BaseType : PTypeInfo;
-                        NameList : ShortString)
+                        NameList : ShortString;
+                        {EnumUnitName: ShortString;})
                     );
                   tkSet:
                     (CompType : PTypeInfo)

commit 05a5e9d903aeade4d495b4feee550c667465e542

compiler: write finished zero for enumeration rtti (this was broken since r14511 fixes bug #0019288)
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@17407 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/ncgrtti.pas
@@ -478,6 +478,8 @@ implementation
             end;
           { write unit name }
           write_string(current_module.realmodulename^);
+          { write zero which is required by RTL }
+          current_asmdata.asmlists[al_rtti].concat(Tai_const.Create_8bit(0));
         end;
 
         procedure orddef_rtti(def:torddef);