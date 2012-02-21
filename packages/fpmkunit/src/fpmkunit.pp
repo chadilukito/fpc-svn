chadilukito/fpc-svn

commit 65cca19ff06475ff178fd1732fa1635e47130aae

* Do not change the current directory, that does not work well with multiple threads
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@20352 3ad0048d-3df7-0310-abae-a5850022a9f2

packages/fpmkunit/src/fpmkunit.pp
@@ -5013,9 +5013,8 @@ begin
   // Recompile a Target of this package?
   If Not Result then
     begin
+      GPathPrefix := APackage.Directory;
       try
-        If (APackage.Directory<>'') then
-          EnterDir(APackage.Directory);
         for i:=0 to APackage.Targets.Count-1 do
           begin
             Result:=NeedsCompile(APackage,APackage.Targets.TargetItems[i]);
@@ -5023,8 +5022,7 @@ begin
               break;
           end;
       Finally
-        If (APackage.Directory<>'') then
-          EnterDir('');
+        GPathPrefix := '';
       end;
     end;
 

commit 1681a60ef1154cc4a434e9ba91590e24fdcbf0f0

* Fixed checking of changed files which got broken in r20352
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@20392 3ad0048d-3df7-0310-abae-a5850022a9f2

packages/fpmkunit/src/fpmkunit.pp
@@ -4797,7 +4797,7 @@ Var
   I : Integer;
   D : TDependency;
   T : TTarget;
-  OD,OFN : String;
+  OD,OFN,TFN : String;
 begin
   Result:=False;
 
@@ -4818,7 +4818,7 @@ begin
         OD:=APackage.GetUnitsOutputDir(Defaults.CPU,Defaults.OS);
       If (OD<>'') then
         OD:=IncludeTrailingPathDelimiter(OD);
-      OFN:=OD+ATarget.GetOutPutFileName(Defaults.OS);
+      OFN:=AddPathPrefix(APackage, OD+ATarget.GetOutPutFileName(Defaults.OS));
       Result:=Not FileExists(OFN);
       if Result then
         Log(vlDebug,SDbgOutputNotYetAvailable,[OFN]);
@@ -4827,8 +4827,9 @@ begin
   // Check main source
   If not Result then
     begin
-      if FileExists(ATarget.TargetSourceFileName) then
-        Result:=FileNewer(ATarget.TargetSourceFileName,OFN)
+      TFN := AddPathPrefix(APackage,ATarget.TargetSourceFileName);
+      if FileExists(TFN) then
+        Result:=FileNewer(TFN,OFN)
     end;
 
   // Check unit and include dependencies