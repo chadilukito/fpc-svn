chadilukito/fpc-svn

commit 27ed28315b12296d5161cf5507b2f5fb9ed7a02e

* Fixed possible AV in DistClean (uninitialized variable)  * Also perform a distclean if a package is not compiled for the current target
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@18060 3ad0048d-3df7-0310-abae-a5850022a9f2

packages/fpmkunit/src/fpmkunit.pp
@@ -5085,8 +5085,8 @@ end;
 
 procedure TBuildEngine.Clean(APackage: TPackage; AllTargets: boolean);
 var
-  ACPU: TCpu;
-  AOS: TOS;
+//  ACPU: TCpu;
+//  AOS: TOS;
   DirectoryList : TStringList;
 begin
   Log(vlInfo,SInfoCleaningPackage,[APackage.Name]);
@@ -5100,7 +5100,7 @@ begin
         // being renamed and such. See also bug 19655
         DirectoryList := TStringList.Create;
         try
-          DirectoryList.Add(ExtractFileDir(APackage.GetUnitsOutputDir(ACPU,AOS)));
+          DirectoryList.Add(ExtractFileDir(APackage.GetUnitsOutputDir(Defaults.CPU,Defaults.OS)));
           CmdRemoveTrees(DirectoryList);
         finally
           DirectoryList.Free;
@@ -5275,7 +5275,7 @@ begin
   For I:=0 to Packages.Count-1 do
     begin
     P:=Packages.PackageItems[i];
-    If PackageOK(P) then
+    If AllTargets or PackageOK(P) then
       Clean(P, AllTargets);
     log(vlWarning, SWarnCleanPackagecomplete, [P.Name]);
     end;

commit 08a87bc8eb88e96130623971b303b6811caea546

* Make distclean should clean up for all known targets. This behavior waw broken since r18060
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@22980 3ad0048d-3df7-0310-abae-a5850022a9f2

packages/fpmkunit/src/fpmkunit.pp
@@ -6227,8 +6227,8 @@ end;
 
 procedure TBuildEngine.Clean(APackage: TPackage; AllTargets: boolean);
 var
-//  ACPU: TCpu;
-//  AOS: TOS;
+  ACPU: TCpu;
+  AOS: TOS;
   DirectoryList : TStringList;
 begin
   Log(vlInfo,SInfoCleaningPackage,[APackage.Name]);
@@ -6242,8 +6242,12 @@ begin
         // being renamed and such. See also bug 19655
         DirectoryList := TStringList.Create;
         try
-          DirectoryList.Add(ExtractFileDir(APackage.GetUnitsOutputDir(Defaults.CPU,Defaults.OS)));
-          DirectoryList.Add(ExtractFileDir(APackage.GetBinOutputDir(Defaults.CPU,Defaults.OS)));
+          for ACPU:=low(TCpu) to high(TCpu) do
+            for AOS:=low(TOS) to high(TOS) do
+              begin
+                DirectoryList.Add(ExtractFileDir(APackage.GetUnitsOutputDir(ACPU,AOS)));
+                DirectoryList.Add(ExtractFileDir(APackage.GetBinOutputDir(ACPU,AOS)));
+              end;
           CmdRemoveTrees(DirectoryList);
         finally
           DirectoryList.Free;