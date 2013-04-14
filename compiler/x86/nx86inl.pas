chadilukito/fpc-svn

commit 019abd52867db37a4ea2111b426ed05881f10150

- removed ti8086inlinenode.second_round_real, second_trunc_real and load_fpu_location as they mostly repeat the code in tx86inlinenode.load_fpu_location; tx86inlinenode.load_fpu_location adapted for i8086 instead
git-svn-id: https://svn.freepascal.org/svn/fpc/branches/i8086@24237 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/x86/nx86inl.pas
@@ -199,27 +199,36 @@ implementation
 
      { load the FPU into the an fpu register }
      procedure tx86inlinenode.load_fpu_location;
+       var
+         lnode: tnode;
        begin
          location_reset(location,LOC_FPUREGISTER,def_cgsize(resultdef));
          location.register:=NR_FPU_RESULT_REG;
-         secondpass(left);
-         case left.location.loc of
+{$ifdef i8086}
+         if left.nodetype <> callparan then
+           internalerror(2013031501);
+         lnode := tcallparanode(left).left;
+{$else i8086}
+         lnode := left;
+{$endif i8086}
+         secondpass(lnode);
+         case lnode.location.loc of
            LOC_FPUREGISTER:
              ;
            LOC_CFPUREGISTER:
              begin
-               cg.a_loadfpu_reg_reg(current_asmdata.CurrAsmList,left.location.size,
-                 left.location.size,left.location.register,location.register);
+               cg.a_loadfpu_reg_reg(current_asmdata.CurrAsmList,lnode.location.size,
+                 lnode.location.size,lnode.location.register,location.register);
              end;
            LOC_REFERENCE,LOC_CREFERENCE:
              begin
                cg.a_loadfpu_ref_reg(current_asmdata.CurrAsmList,
-                  left.location.size,left.location.size,
-                  left.location.reference,location.register);
+                  lnode.location.size,lnode.location.size,
+                  lnode.location.reference,location.register);
              end;
            LOC_MMREGISTER,LOC_CMMREGISTER:
              begin
-               location:=left.location;
+               location:=lnode.location;
                location_force_fpureg(current_asmdata.CurrAsmList,location,false);
              end;
            else

compiler/i8086/n8086inl.pas
@@ -33,14 +33,9 @@ interface
        { ti8086inlinenode }
 
        ti8086inlinenode = class(tx86inlinenode)
-         procedure second_round_real; override;
-         procedure second_trunc_real; override;
-
          function typecheck_seg: tnode; override;
          function first_seg: tnode; override;
          procedure second_seg; override;
-       private
-         procedure load_fpu_location;
        end;
 
 implementation
@@ -61,37 +56,6 @@ implementation
     cga,cgutils,cgx86,cgobj,hlcgobj,
     htypechk;
 
-     procedure ti8086inlinenode.second_round_real;
-       begin
-         load_fpu_location;
-         location_reset_ref(location,LOC_REFERENCE,OS_S64,0);
-         tg.GetTemp(current_asmdata.CurrAsmList,resultdef.size,resultdef.alignment,tt_normal,location.reference);
-         emit_ref(A_FISTP,S_IQ,location.reference);
-         tcgx86(cg).dec_fpu_stack;
-         emit_none(A_FWAIT,S_NO);
-       end;
-
-     procedure ti8086inlinenode.second_trunc_real;
-       var
-         oldcw,newcw : treference;
-       begin
-         tg.GetTemp(current_asmdata.CurrAsmList,2,2,tt_normal,oldcw);
-         tg.GetTemp(current_asmdata.CurrAsmList,2,2,tt_normal,newcw);
-         emit_ref(A_FNSTCW,S_NO,newcw);
-         emit_ref(A_FNSTCW,S_NO,oldcw);
-         emit_const_ref(A_OR,S_W,$0f00,newcw);
-         load_fpu_location;
-         emit_ref(A_FLDCW,S_NO,newcw);
-         location_reset_ref(location,LOC_REFERENCE,OS_S64,0);
-         tg.GetTemp(current_asmdata.CurrAsmList,resultdef.size,resultdef.alignment,tt_normal,location.reference);
-         emit_ref(A_FISTP,S_IQ,location.reference);
-         tcgx86(cg).dec_fpu_stack;
-         emit_ref(A_FLDCW,S_NO,oldcw);
-         emit_none(A_FWAIT,S_NO);
-         tg.UnGetTemp(current_asmdata.CurrAsmList,oldcw);
-         tg.UnGetTemp(current_asmdata.CurrAsmList,newcw);
-       end;
-
      function ti8086inlinenode.typecheck_seg: tnode;
        begin
          result := nil;
@@ -109,40 +73,6 @@ implementation
          location_reset(location,LOC_REGISTER,OS_16);
          location.register:=cg.getintregister(current_asmdata.CurrAsmList,OS_16);
          current_asmdata.CurrAsmList.Concat(Taicpu.op_reg_reg(A_MOV,S_W,NR_DS,location.register));
-       end;
-
-     procedure ti8086inlinenode.load_fpu_location;
-       var
-         lnode: tnode;
-       begin
-         location_reset(location,LOC_FPUREGISTER,def_cgsize(resultdef));
-         location.register:=NR_FPU_RESULT_REG;
-         if left.nodetype <> callparan then
-           internalerror(2013031501);
-         lnode := tcallparanode(left).left;
-         secondpass(lnode);
-         case lnode.location.loc of
-           LOC_FPUREGISTER:
-             ;
-           LOC_CFPUREGISTER:
-             begin
-               cg.a_loadfpu_reg_reg(current_asmdata.CurrAsmList,lnode.location.size,
-                 lnode.location.size,lnode.location.register,location.register);
-             end;
-           LOC_REFERENCE,LOC_CREFERENCE:
-             begin
-               cg.a_loadfpu_ref_reg(current_asmdata.CurrAsmList,
-                  lnode.location.size,lnode.location.size,
-                  lnode.location.reference,location.register);
-             end;
-           LOC_MMREGISTER,LOC_CMMREGISTER:
-             begin
-               location:=lnode.location;
-               location_force_fpureg(current_asmdata.CurrAsmList,location,false);
-             end;
-           else
-             internalerror(309991);
-         end;
        end;
 
 begin

commit 0bbefd9ffd68d4e4b5b6066abdb6096347c07a4c

* fixed bugs in tx86inlinenode, introduced in r24237
git-svn-id: https://svn.freepascal.org/svn/fpc/branches/i8086@24243 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/x86/nx86inl.pas
@@ -65,7 +65,7 @@ interface
 {$endif not i8086}
           procedure second_popcnt;override;
        private
-          procedure load_fpu_location;
+          procedure load_fpu_location(lnode: tnode);
        end;
 
 implementation
@@ -198,19 +198,10 @@ implementation
        end;
 
      { load the FPU into the an fpu register }
-     procedure tx86inlinenode.load_fpu_location;
-       var
-         lnode: tnode;
+     procedure tx86inlinenode.load_fpu_location(lnode: tnode);
        begin
          location_reset(location,LOC_FPUREGISTER,def_cgsize(resultdef));
          location.register:=NR_FPU_RESULT_REG;
-{$ifdef i8086}
-         if left.nodetype <> callparan then
-           internalerror(2013031501);
-         lnode := tcallparanode(left).left;
-{$else i8086}
-         lnode := left;
-{$endif i8086}
          secondpass(lnode);
          case lnode.location.loc of
            LOC_FPUREGISTER:
@@ -239,7 +230,7 @@ implementation
 
      procedure tx86inlinenode.second_arctan_real;
        begin
-         load_fpu_location;
+         load_fpu_location(left);
          emit_none(A_FLD1,S_NO);
          emit_none(A_FPATAN,S_NO);
        end;
@@ -267,7 +258,7 @@ implementation
            end
          else
            begin
-             load_fpu_location;
+             load_fpu_location(left);
              emit_none(A_FABS,S_NO);
            end;
        end;
@@ -294,7 +285,13 @@ implementation
          else
 {$endif x86_64}
           begin
-            load_fpu_location;
+{$ifdef i8086}
+            if left.nodetype <> callparan then
+              internalerror(2013031501);
+            load_fpu_location(tcallparanode(left).left);
+{$else i8086}
+            load_fpu_location(left);
+{$endif i8086}
             location_reset_ref(location,LOC_REFERENCE,OS_S64,0);
             tg.GetTemp(current_asmdata.CurrAsmList,resultdef.size,resultdef.alignment,tt_normal,location.reference);
             emit_ref(A_FISTP,S_IQ,location.reference);
@@ -330,7 +327,13 @@ implementation
           begin
             if (current_settings.fputype>=fpu_sse3) then
               begin
-                load_fpu_location;
+{$ifdef i8086}
+                if left.nodetype <> callparan then
+                  internalerror(2013031501);
+                load_fpu_location(tcallparanode(left).left);
+{$else i8086}
+                load_fpu_location(left);
+{$endif i8086}
                 location_reset_ref(location,LOC_REFERENCE,OS_S64,0);
                 tg.GetTemp(current_asmdata.CurrAsmList,resultdef.size,resultdef.alignment,tt_normal,location.reference);
                 emit_ref(A_FISTTP,S_IQ,location.reference);
@@ -343,7 +346,11 @@ implementation
                 emit_ref(A_FNSTCW,S_NO,newcw);
                 emit_ref(A_FNSTCW,S_NO,oldcw);
                 emit_const_ref(A_OR,S_W,$0f00,newcw);
-                load_fpu_location;
+{$ifdef i8086}
+                load_fpu_location(tcallparanode(left).left);
+{$else i8086}
+                load_fpu_location(left);
+{$endif i8086}
                 emit_ref(A_FLDCW,S_NO,newcw);
                 location_reset_ref(location,LOC_REFERENCE,OS_S64,0);
                 tg.GetTemp(current_asmdata.CurrAsmList,resultdef.size,resultdef.alignment,tt_normal,location.reference);
@@ -370,7 +377,7 @@ implementation
            end
          else
            begin
-             load_fpu_location;
+             load_fpu_location(left);
              emit_reg_reg(A_FMUL,S_NO,NR_ST0,NR_ST0);
            end;
        end;
@@ -394,14 +401,14 @@ implementation
            end
          else
            begin
-             load_fpu_location;
+             load_fpu_location(left);
              emit_none(A_FSQRT,S_NO);
            end;
        end;
 
      procedure tx86inlinenode.second_ln_real;
        begin
-         load_fpu_location;
+         load_fpu_location(left);
          emit_none(A_FLDLN2,S_NO);
          emit_none(A_FXCH,S_NO);
          emit_none(A_FYL2X,S_NO);
@@ -409,13 +416,13 @@ implementation
 
      procedure tx86inlinenode.second_cos_real;
        begin
-         load_fpu_location;
+         load_fpu_location(left);
          emit_none(A_FCOS,S_NO);
        end;
 
      procedure tx86inlinenode.second_sin_real;
        begin
-         load_fpu_location;
+         load_fpu_location(left);
          emit_none(A_FSIN,S_NO)
        end;
 