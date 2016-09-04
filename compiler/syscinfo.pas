chadilukito/fpc-svn

commit 14898f4d83bf77a1ab0cdd1f478783a3b1ba6bda

huge syscall support refactor for Amiga-likes. removed large chunks of ancient duplicated code, and in general tried to make the entire thing more maintainable and cleaner. also added support for AROS EAXBase syscall convention
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@34416 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/syscinfo.pas
@@ -1,0 +1,131 @@
+{
+    Copyright (c) 2016 by Karoly Balogh
+
+    Contains information on syscalls
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+ ****************************************************************************
+}
+unit syscinfo;
+
+{$i fpcdefs.inc}
+
+interface
+
+uses
+  globtype, systems, tokens, symconst;
+
+type
+  tsyscallinfo = record
+    token: ttoken;
+    procoption: tprocoption;
+    validon: set of tsystem;
+  end;
+  psyscallinfo = ^tsyscallinfo;
+
+const
+  syscall_conventions: array[1..7] of tsyscallinfo = (
+      ( token: _LEGACY;    procoption: po_syscall_legacy;    validon: [system_powerpc_morphos,system_m68k_amiga] ),
+      ( token: _SYSV;      procoption: po_syscall_sysv;      validon: [system_powerpc_morphos] ),
+      ( token: _SYSVBASE;  procoption: po_syscall_sysvbase;  validon: [system_powerpc_morphos] ),
+      ( token: _BASESYSV;  procoption: po_syscall_basesysv;  validon: [system_powerpc_morphos,system_powerpc_amiga] ),
+      ( token: _R12BASE;   procoption: po_syscall_r12base;   validon: [system_powerpc_morphos] ),
+      ( token: _STACKBASE; procoption: po_syscall_stackbase; validon: [system_i386_aros,system_x86_64_aros] ),
+      ( token: _EAXBASE;   procoption: po_syscall_eaxbase;   validon: [system_i386_aros,system_x86_64_aros] ));
+
+
+function get_syscall_by_token(const token: ttoken): psyscallinfo;
+function get_syscall_by_name(const name: string): psyscallinfo;
+function get_default_syscall: tprocoption;
+procedure set_default_syscall(sc: tprocoption);
+
+implementation
+
+uses
+  verbose;
+
+const
+  syscall_conventions_po = [ po_syscall_legacy, po_syscall_sysv, po_syscall_sysvbase, po_syscall_basesysv,
+                             po_syscall_r12base, po_syscall_stackbase, po_syscall_eaxbase ];
+
+type
+  tsyscalldefaultinfo = record
+    system: tsystem;
+    procoption: tprocoption;
+  end;
+
+const
+  default_syscall_conventions: array[0..4] of tsyscalldefaultinfo = (
+      ( system: system_m68k_amiga;      procoption: po_syscall_legacy ),
+      ( system: system_powerpc_amiga;   procoption: po_syscall_basesysv ),
+      ( system: system_powerpc_morphos; procoption: po_syscall_legacy ),
+      ( system: system_i386_aros;       procoption: po_syscall_stackbase ),
+      ( system: system_x86_64_aros;     procoption: po_syscall_stackbase ));
+
+var
+  default_syscall_convention: tprocoption = po_none;
+
+function get_syscall_by_token(const token: ttoken): psyscallinfo;
+var
+  i: aint;
+begin
+  result:=nil;
+  for i:=low(syscall_conventions) to high(syscall_conventions) do
+    if syscall_conventions[i].token = token then
+      begin
+        result:=@syscall_conventions[i];
+        break;
+      end;
+end;
+
+function get_syscall_by_name(const name: string): psyscallinfo;
+var
+  i: aint;
+begin
+  result:=nil;
+  for i:=low(syscall_conventions) to high(syscall_conventions) do
+    if arraytokeninfo[syscall_conventions[i].token].str = name then
+      begin
+        result:=@syscall_conventions[i];
+        break;
+      end;
+end;
+
+function get_default_syscall: tprocoption;
+var
+  i: aint;
+begin
+  if not (default_syscall_convention in syscall_conventions_po) then
+    begin
+      for i:=low(default_syscall_conventions) to high(default_syscall_conventions) do
+        if default_syscall_conventions[i].system = target_info.system then
+          default_syscall_convention:=default_syscall_conventions[i].procoption;
+      if not (default_syscall_convention in syscall_conventions_po) then
+        internalerror(2016090302);
+    end;
+
+  result:=default_syscall_convention;
+end;
+
+procedure set_default_syscall(sc: tprocoption);
+begin
+  if not (sc in syscall_conventions_po) then
+    internalerror(2016090301);
+
+  default_syscall_convention:=sc;
+end;
+
+end.

.gitattributes
@@ -707,6 +707,7 @@ compiler/symsym.pas svn_mime_002dtype=text%2Fplain svn_eol_002dstyle=native
 compiler/symtable.pas svn_mime_002dtype=text%2Fplain svn_eol_002dstyle=native
 compiler/symtype.pas svn_mime_002dtype=text%2Fplain svn_eol_002dstyle=native
 compiler/symutil.pas svn_mime_002dtype=text%2Fplain svn_eol_002dstyle=native
+compiler/syscinfo.pas svn_mime_002dtype=text%2Fplain svn_eol_002dstyle=native
 compiler/systems.inc svn_mime_002dtype=text%2Fplain svn_eol_002dstyle=native
 compiler/systems.pas svn_mime_002dtype=text%2Fplain svn_eol_002dstyle=native
 compiler/systems/i_aix.pas svn_mime_002dtype=text%2Fplain svn_eol_002dstyle=native

compiler/globals.pas
@@ -368,11 +368,6 @@ interface
        palmos_applicationid : string[4] = 'FPCA';
 {$endif defined(m68k) or defined(arm)}
 
-{$ifdef powerpc}
-       { default calling convention used on MorphOS }
-       syscall_convention : string = 'LEGACY';
-{$endif powerpc}
-
        { default name of the C-style "main" procedure of the library/program }
        { (this will be prefixed with the target_info.cprefix)                }
        defaultmainaliasname = 'main';

compiler/i386/cpupara.pas
@@ -44,6 +44,7 @@ unit cpupara;
           function create_varargs_paraloc_info(p : tabstractprocdef; varargspara:tvarargsparalist):longint;override;
           procedure createtempparaloc(list: TAsmList;calloption : tproccalloption;parasym : tparavarsym;can_use_final_stack_loc : boolean;var cgpara:TCGPara);override;
           function get_funcretloc(p : tabstractprocdef; side: tcallercallee; forcetempdef: tdef): TCGPara;override;
+          function parseparaloc(p : tparavarsym;const s : string) : boolean;override;
        private
           procedure create_stdcall_paraloc_info(p : tabstractprocdef; side: tcallercallee;paras:tparalist;var parasize:longint);
           procedure create_register_paraloc_info(p : tabstractprocdef; side: tcallercallee;paras:tparalist;var parareg,parasize:longint);
@@ -53,7 +54,7 @@ unit cpupara;
   implementation
 
     uses
-       cutils,
+       cutils,sysutils,
        systems,verbose,
        symtable,
        defutil;
@@ -286,6 +287,32 @@ unit cpupara;
       end;
 
 
+    function tcpuparamanager.parseparaloc(p : tparavarsym;const s : string) : boolean;
+      var
+        paraloc : pcgparalocation;
+      begin
+        result:=false;
+        case target_info.system of
+          system_i386_aros:
+            begin
+              p.paraloc[callerside].alignment:=4;
+              paraloc:=p.paraloc[callerside].add_location;
+              paraloc^.loc:=LOC_REGISTER;
+              paraloc^.size:=def_cgsize(p.vardef);
+              paraloc^.def:=p.vardef;
+              paraloc^.register:=std_regnum_search(lowercase(s));
+              if paraloc^.register = NR_NO then
+                exit;
+
+              { copy to callee side }
+              p.paraloc[calleeside].add_location^:=paraloc^;
+            end;
+          else
+            internalerror(2016090103);
+        end;
+        result:=true;
+      end;
+
     function  tcpuparamanager.get_funcretloc(p : tabstractprocdef; side: tcallercallee; forcetempdef: tdef): TCGPara;
       var
         retcgsize  : tcgsize;
@@ -417,6 +444,19 @@ unit cpupara;
           begin
             hp:=tparavarsym(paras[i]);
             paradef:=hp.vardef;
+
+            { syscall for AROS can have already a paraloc set }
+            if (vo_has_explicit_paraloc in hp.varoptions) then
+              begin
+                if not(vo_is_syscall_lib in hp.varoptions) then
+                  internalerror(2016090105);
+                if p.proccalloption in pushleftright_pocalls then
+                  dec(i)
+                else
+                  inc(i);
+                continue;
+              end;
+
             pushaddr:=push_addr_param(hp.varspez,paradef,p.proccalloption);
             if pushaddr then
               begin

compiler/i386/n386cal.pas
@@ -50,7 +50,8 @@ implementation
       cpubase,paramgr,
       aasmtai,aasmdata,aasmcpu,
       nbas,nmem,nld,ncnv,
-      symdef,symsym,symcpu,
+      parabase,
+      symdef,symsym,symcpu,symconst,
       cga,cgobj,cpuinfo;
 
 
@@ -62,30 +63,42 @@ implementation
     procedure ti386callnode.do_syscall;
       var
         tmpref: treference;
+        libparaloc: pcgparalocation;
       begin
         case target_info.system of
           system_i386_aros:
             begin
-              // one syscall convention for AROS
-              current_asmdata.CurrAsmList.concat(tai_comment.create(strpnew('AROS SysCall')));
-              reference_reset(tmpref,sizeof(pint));
-              { re-read the libbase pushed first on the stack, instead of just trusting the
-                mangledname will work. this is important for example for threadvar libbases.
-                and this way they also don't need to be resolved twice then. (KB) }
-              tmpref.base:=NR_ESP;
-              tmpref.offset:=pushedparasize-sizeof(pint);
-              cg.getcpuregister(current_asmdata.CurrAsmList,NR_EAX);
-              cg.a_load_ref_reg(current_asmdata.CurrAsmList,OS_ADDR,OS_ADDR,tmpref,NR_EAX);
-              reference_reset_base(tmpref,NR_EAX,-tprocdef(procdefinition).extnumber,sizeof(pint));
-              cg.a_load_ref_reg(current_asmdata.CurrAsmList,OS_ADDR,OS_ADDR,tmpref,NR_EAX);
-              cg.a_call_reg(current_asmdata.CurrAsmList,NR_EAX);
-              cg.ungetcpuregister(current_asmdata.CurrAsmList,NR_EAX);
+              if (po_syscall_stackbase in tprocdef(procdefinition).procoptions) then
+                begin
+                  current_asmdata.CurrAsmList.concat(tai_comment.create(strpnew('AROS SysCall - StackBase')));
+                  { re-read the libbase pushed first on the stack, instead of just trusting the
+                    mangledname will work. this is important for example for threadvar libbases.
+                    and this way they also don't need to be resolved twice then. (KB) }
+                  libparaloc:=paralocs[procdefinition.paras.count-1]^.location;
+                  if libparaloc^.loc <> LOC_REFERENCE then
+                    internalerror(2016090203);
+                  reference_reset_base(tmpref,libparaloc^.reference.index,libparaloc^.reference.offset,sizeof(pint));
+                  cg.getcpuregister(current_asmdata.CurrAsmList,NR_EAX);
+                  cg.a_load_ref_reg(current_asmdata.CurrAsmList,OS_ADDR,OS_ADDR,tmpref,NR_EAX);
+                  reference_reset_base(tmpref,NR_EAX,-tprocdef(procdefinition).extnumber,sizeof(pint));
+                  current_asmdata.CurrAsmList.concat(taicpu.op_ref(A_CALL,S_NO,tmpref));
+                  cg.ungetcpuregister(current_asmdata.CurrAsmList,NR_EAX);
+                  exit;
+                end;
+             if (po_syscall_eaxbase in tprocdef(procdefinition).procoptions) then
+                begin
+                  current_asmdata.CurrAsmList.concat(tai_comment.create(strpnew('AROS SysCall - EAXBase')));
+                  { libbase must be in EAX already, so just piggyback that, and dereference it }
+                  reference_reset_base(tmpref,NR_EAX,-tprocdef(procdefinition).extnumber,sizeof(pint));
+                  current_asmdata.CurrAsmList.concat(taicpu.op_ref(A_CALL,S_NO,tmpref));
+                  exit;
+                end;
+              internalerror(2016090104);
             end;
           else
             internalerror(2014081801);
         end;
       end;
-
 
     procedure ti386callnode.gen_syscall_para(para: tcallparanode);
       begin

compiler/msg/errore.msg
@@ -139,7 +139,7 @@ general_e_exception_raised=01026_E_Compilation raised exception internally
 #
 # Scanner
 #
-# 02099 is the last used one
+# 02101 is the last used one
 #
 % \section{Scanner messages.}
 % This section lists the messages that the scanner emits. The scanner takes
@@ -413,6 +413,11 @@ scan_e_illegal_hugepointernormalization=02098_E_Illegal argument for HUGEPOINTER
 scan_e_illegal_asmcpu_specifier=02099_E_Illegal assembler CPU instruction set specified "$1"
 % When you specify an assembler CPU with the \var{\{\$ASMCPU xxx\}} directive,
 % the compiler didn't recognize the CPU you specified.
+scan_w_syscall_convention_not_useable_on_target=02100_W_Specified syscall convention is not useable on this target
+% The specified syscall convention using the \var{\{\$SYSCALL xxx\}} directive,
+% is not useable on the current target system.
+scan_w_syscall_convention_invalid=02101_W_Invalid syscall convention specified
+% The compiler did not recognize the syscall convention specified by the \var{\{\$SYSCALL xxx\}} directive.
 % \end{description}
 #
 # Parser

compiler/msgidx.inc
@@ -122,6 +122,8 @@ const
   scan_w_heapmax_lessthan_heapmin=02097;
   scan_e_illegal_hugepointernormalization=02098;
   scan_e_illegal_asmcpu_specifier=02099;
+  scan_w_syscall_convention_not_useable_on_target=02100;
+  scan_w_syscall_convention_invalid=02101;
   parser_e_syntax_error=03000;
   parser_e_dont_nest_interrupt=03004;
   parser_w_proc_directive_ignored=03005;
@@ -1059,9 +1061,9 @@ const
   option_info=11024;
   option_help_pages=11025;
 
-  MsgTxtSize = 78269;
+  MsgTxtSize = 78381;
 
   MsgIdxMax : array[1..20] of longint=(
-    27,100,347,124,96,58,130,33,208,65,
+    27,102,347,124,96,58,130,33,208,65,
     58,20,30,1,1,1,1,1,1,1
   );

commit 41d921f7ff038ae355abdf2d0df4706fe3435519

fixed builds on 32bit hosts to 64bit targets (broken by r34416)
git-svn-id: https://svn.freepascal.org/svn/fpc/trunk@34421 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/syscinfo.pas
@@ -80,7 +80,7 @@ var
 
 function get_syscall_by_token(const token: ttoken): psyscallinfo;
 var
-  i: aint;
+  i: longint;
 begin
   result:=nil;
   for i:=low(syscall_conventions) to high(syscall_conventions) do
@@ -93,7 +93,7 @@ end;
 
 function get_syscall_by_name(const name: string): psyscallinfo;
 var
-  i: aint;
+  i: longint;
 begin
   result:=nil;
   for i:=low(syscall_conventions) to high(syscall_conventions) do
@@ -106,7 +106,7 @@ end;
 
 function get_default_syscall: tprocoption;
 var
-  i: aint;
+  i: longint;
 begin
   if not (default_syscall_convention in syscall_conventions_po) then
     begin